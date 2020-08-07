be466e0d6ff226846e96f5b543cde9b4bbac096d
jtoivo <69200607+jtoivo@users.noreply.github.com>
2020-08-07 10:41:11 +0300

    Add files via upload

---
+++ osa1/kurssitiedot/src/index.js
@@ -0,0 +1,60 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Header = ({ name }) => {
+  return <h1>{name}</h1>;
+};
+
+const Part = ({ part }) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  );
+};
+
+const Content = ({ course }) => {
+  return (
+    <div>
+      <Part part={course.parts[0]} />
+      <Part part={course.parts[1]} />
+      <Part part={course.parts[2]} />
+    </div>
+  );
+};
+
+const Total = (props) => {
+  let sum = 0;
+  props.course.parts.forEach((part) => (sum += part.exercises));
+  return <p>Number of exercises {sum}</p>;
+};
+
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <Header name={course.name} />
+      <Content course={course} />
+      <Total course={course} />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
