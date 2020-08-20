5c8657540d525438882eaa158b6d4dbfd983c706
jtoivo <jiitee1979@gmail.com>
2020-08-20 13:59:57 +0300

    Servicen baseUrl:a muokattu

--- osa2/puhelinluettelo/src/services/personService.js
+++ osa2/puhelinluettelo/src/services/personService.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseUrl = "https://murmuring-mountain-42851.herokuapp.com/api/persons";
+const baseUrl = "/api/persons";
 
 const getAll = () => {
   const request = axios.get(baseUrl);
