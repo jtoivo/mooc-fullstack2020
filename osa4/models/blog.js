c4112b793f6cff7b8df2351132c5491bb58b62a2
jtoivo <jiitee1979@gmail.com>
2020-08-25 10:30:02 +0300

    Tehtävä 4.2 valmis.

---
+++ osa4/models/blog.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+
+blogSchema.set('toJSON', {
+  transform: (document, returnObj) => {
+    returnObj.id = returnObj._id.toString()
+    delete returnObj._id
+    delete returnObj.__v
+  }
+})
+
+module.exports = mongoose.model('Blog', blogSchema)
