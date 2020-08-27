952cbf0b81296ea83c7a6bd6f5a0a98b9eda32bf
jtoivo <jiitee1979@gmail.com>
2020-08-27 15:31:02 +0300

    Tehtävä 4.12 valmis.

--- osa4/controllers/blogs.js
+++ osa4/controllers/blogs.js
@@ -3,11 +3,16 @@ const Blog = require('../models/blog')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({})
-  response.json(blogs.map(blog => blog.toJSON()))
+  response.json(blogs.map(b => b.toJSON()))
 })
 
 blogsRouter.post('/', async (request, response) => {
   const blog = new Blog(request.body)
+
+  if (blog.likes === undefined) blog.likes = 0
+
+  if (blog.title === undefined || blog.url === undefined) response.status(400)
+
   const savedBlog = await blog.save()
   response.json(savedBlog.toJSON())
 })
--- osa4/tests/blog_api.test.js
+++ osa4/tests/blog_api.test.js
@@ -1,35 +1,110 @@
 const supertest = require('supertest')
 const mongoose = require('mongoose')
+const helper = require('./test_helper')
 const app = require('../app')
 const api = supertest(app)
 const Blog = require('../models/blog')
 
-const initialBlogs = [
-  { title: 'React patterns', author: 'Michael Chan', url: 'https://reactpatterns.com/', likes: 7 },
-  { title: 'Go To Statement Considered Harmful', author: 'Edsger W. Dijkstra', url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html', likes: 5 },
-  { title: 'Canonical string reduction', author: 'Edsger W. Dijkstra', url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html', likes: 12 },
-  { title: 'First class tests', author: 'Robert C. Martin', url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll', likes: 10 },
-  { title: 'TDD harms architecture', author: 'Robert C. Martin', url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html', likes: 0 },
-  { title: 'Type wars', author: 'Robert C. Martin', url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html', likes: 2 }
-]
-
-beforeEach(async () => {
+beforeAll(async () => {
   jest.setTimeout(20000)
-
-  await Blog.deleteMany({})
-
-  let blogObj = new Blog(initialBlogs[0])
-  await blogObj.save()
-
-  blogObj = new Blog(initialBlogs[1])
-  await blogObj.save()
 })
 
-test('blogs returned as json', async () => {
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  await Blog.insertMany(helper.initialBlogs)
+})
+
+test('blogs are returned as json', async () => {
   await api
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
+})
+
+test('all blogs are returned', async () => {
+  const response = await api.get('/api/blogs')
+  expect(response.body).toHaveLength(helper.initialBlogs.length)
+})
+
+test('there are 6 blogs', async () => {
+  const response = await api.get('/api/blogs')
+  expect(response.body).toHaveLength(6)
+})
+
+test('the first is about React patterns', async () => {
+  const response = await api.get('/api/blogs')
+  expect(response.body[0].title).toBe('React patterns')
+})
+
+test('a specified blog is within the returned ones', async () => {
+  const response = await api.get('/api/blogs')
+  const titles = response.body.map(b => b.title)
+  expect(titles).toContain('Type wars')
+})
+
+test('id field is defined', async () => {
+  const response = await api.get('/api/blogs')
+  expect(response.body[0].id).toBeDefined()
+})
+
+test('a valid blog can be added', async () => {
+  const newBlog = {
+    title: 'A new title',
+    author: 'Some Name',
+    url: 'qwerty',
+    likes: 0
+  }
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsInDb = await helper.blogsInDb()
+  expect(blogsInDb).toHaveLength(helper.initialBlogs.length + 1)
+
+  const titles = blogsInDb.map(b => b.title)
+  expect(titles).toContain('A new title')
+})
+
+test('a new blog with missing likes will get default 0 likes', async () => {
+  const newBlog = {
+    title: 'A blog without likes property',
+    author: 'Some Name',
+    url: 'qwerty',
+  }
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsInDb = await helper.blogsInDb()
+  expect(blogsInDb).toHaveLength(helper.initialBlogs.length + 1)
+
+  const addedBlog = blogsInDb.find(b => b.title === 'A blog without likes property')
+  expect(addedBlog.likes).toBe(0)
+})
+
+test('missing title will return 400', async () => {
+  const newBlog = {
+    author: 'No Title',
+    url: 'fdsfds',
+  }
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(400)
+})
+test('missing url will return 400', async () => {
+  const newBlog = {
+    title: 'Missing url',
+    author: 'Hiuy Fyuh',
+  }
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(400)
 })
 
 afterAll(async () => {
