dbfac90873f2fee4133cd2dad24d8f5a2a63e902
jtoivo <jiitee1979@gmail.com>
2020-08-28 16:21:23 +0300

    Tehtävä 4.17 valmis

--- osa4/controllers/blogs.js
+++ osa4/controllers/blogs.js
@@ -1,31 +1,43 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog
+    .find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs.map(b => b.toJSON()))
 })
 
 blogsRouter.post('/', async (request, response) => {
-  const blog = new Blog(request.body)
-
-  if (blog.likes === undefined)
-    blog.likes = 0
+  const body = request.body
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes === undefined ? 0 : body.likes
+  })
 
   if (blog.title === undefined || blog.url === undefined)
     response.status(400)
 
+  const user = (await User.find({}))[0]
+  blog.user = user._id
   const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
   response.json(savedBlog.toJSON())
 })
 
 blogsRouter.put('/:id', async (request, response) => {
   const body = request.body
+
+
   const blog = {
     title: body.title,
     author: body.author,
     url: body.url,
-    likes: body.likes
+    likes: body.likes,
   }
   const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
   response.json(updatedBlog.toJSON())
