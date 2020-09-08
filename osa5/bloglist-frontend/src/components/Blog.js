024d61c2b9eb062c961cebbc7ef44764dc60176b
jtoivo <jiitee1979@gmail.com>
2020-09-08 16:29:29 +0300

    Tehtävä 5.7 valmis

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,34 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false)
+  const [buttonText, setButtonText] = useState('view')
+
+  const detailsVisibility = { display: showDetails ? '' : 'none' }
+
+  const showOrHide = (target) => {
+    setShowDetails(!showDetails)
+    setButtonText(showDetails ? 'view' : 'hide')
+  }
+
+  const blogStyle = {
+    padding: 8,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author}
+      <button onClick={showOrHide}>{buttonText}</button>
+      <div style={detailsVisibility}>
+        {blog.url}<br />
+        {blog.likes} <button>like</button><br />
+        {blog.user.name}
+      </div >
+    </div>
+  )
+}
 
 export default Blog
