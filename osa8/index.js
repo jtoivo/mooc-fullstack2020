bd9dd682e6ef087919e7a35cf7a8974beadc4176
jtoivo <jiitee1979@gmail.com>
2020-09-24 15:34:33 +0300

    Tehtävä 8.7 valmis

--- osa8/index.js
+++ osa8/index.js
@@ -114,6 +114,11 @@ const typeDefs = gql`
       published: Int!,
       genres: [String!]!
     ) : Book
+
+    editAuthor(
+      name: String!
+      setBornTo: Int!
+    ) : Author
   }
 `
 
@@ -147,6 +152,15 @@ const resolvers = {
       }
 
       return book
+    },
+    editAuthor: (root, args) => {
+      const author = authors.find(a => a.name === args.name)
+      if (!author)
+        return null
+
+      author.born = args.setBornTo
+      authors = authors.map(a => a.name === author.name ? author : a)
+      return author
     }
   }
 }
